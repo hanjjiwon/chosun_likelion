e977fd89b1216ef61605229e81ba6959be6f8214
jiwon <tjfdnjs0829@naver.com>
2020-02-21 18:52:06 +0900

    write

--- mainproject/write/views.py
+++ mainproject/write/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
-
+def write(request):
+    return render(request, 'write.html')
