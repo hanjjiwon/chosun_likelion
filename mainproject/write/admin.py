31bfde975332aad277c80c44d30c445d616afc74
jiwon <tjfdnjs0829@naver.com>
2020-02-22 19:21:07 +0900

    db_add

--- mainproject/write/admin.py
+++ mainproject/write/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-
+from write.models import Subject_range, Subject_code, Subject, Evaluation
 
 # Register your models here.
+
+admin.site.register(Subject_range)
+admin.site.register(Subject_code)
+admin.site.register(Subject)
+admin.site.register(Evaluation)
