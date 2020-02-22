46b63c90ca08ec56347ae0251feaa0339f52f707
jiyooooo <49031330+jiyooooo@users.noreply.github.com>
2020-02-22 21:12:57 +0900

    로그인하고 유저가입하면 리다이렉트 돼야하는데 안됨

--- mainproject/account/views.py
+++ mainproject/account/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import login, authenticate
 
-from django.http import HttpResponse
 
 
 def signup(request):
