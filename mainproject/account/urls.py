9f468fa72f207d2d3765522dec4f708297817db2
jiyooooo <49031330+jiyooooo@users.noreply.github.com>
2020-02-22 14:02:35 +0900

    0222

--- mainproject/account/urls.py
+++ mainproject/account/urls.py
@@ -6,6 +6,6 @@ app_name = "account"
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='account/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('signup/', signup, name='signup'),
 ]
