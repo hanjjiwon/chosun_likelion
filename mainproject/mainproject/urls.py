72133e13c7d920dd8a48048ae44a8eae5b798bfe
jiwon <tjfdnjs0829@naver.com>
2020-02-22 20:47:47 +0900

    wow

--- mainproject/mainproject/urls.py
+++ mainproject/mainproject/urls.py
@@ -20,7 +20,6 @@ import mainapp.views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',mainapp.views.home, name="home"),
--- mainproject/write/admin.py
+++ mainproject/write/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-
+from write.models import Subject_range, Subject_code, Subject, Evaluation,Write_index
 
 # Register your models here.
+admin.site.register(Subject_range)
+admin.site.register(Subject_code)
+admin.site.register(Subject)
+admin.site.register(Evaluation)
+admin.site.register(Write_index)
