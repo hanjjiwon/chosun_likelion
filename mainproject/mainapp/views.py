93def89ff36715d4ea608c08e2cc0017a583921c
jmy <mickey0711@likelion.org>
2020-02-23 18:23:35 +0900

    view

--- mainproject/mainapp/views.py
+++ mainproject/mainapp/views.py
@@ -6,11 +6,18 @@ def index(request):
     return render(request, 'index.html')
 
 def searchlist(request):
-    searchtext=request.GET.get('searchtext',' ')
+    searchtext=request.GET.get('searchtext','')
     subject=Subject.objects.all()
 
     if not searchtext == '':
-        subject=subject.filter(subject_name__icontains=searchtext)
-        return render(request, 'searchlist.html', {'subject':subject})
+        subject=subject.filter(subject_name=searchtext)
+        # subjectrange=subject.filter(subject_range=searchtext)
+        # if not subject == '':
+        #     return render(request, 'searchlist.html', {'subject':subject, 'searchtext':searchtext})
+        # elif not subjectrange =='':
+        #     return render(request, 'searchlist.html', {'subjectrange':subjectrange})
+        # # else:
+        #     return redirect('index')
+        return render(request, 'searchlist.html', {'subject':subject, 'searchtext':searchtext})
     else:
         return redirect('index')
